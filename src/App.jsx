aacde6d1969e1b19eb23fbab6e0b01cb20485b00
leonard <leonardgre@gmail.com>
2023-06-16 20:48:02 +0000

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Skills from "./components/skills";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
-
 function App() {
-
   return (
     <div className=" ">
-    <div className=" font-test px-6 sm:px-10 lg:px-[165px] bg-primary-dark text-primary-white">
-      <Header />
-      <Hero />
-      <Skills />
-      <Projects/>
- 
+      <div className=" font-test px-6 sm:px-10 lg:px-[165px] bg-primary-dark text-primary-white">
+        <Header />
+        <Hero />
+        <Skills />
+        <Projects />
+      </div>
+      <Contact />
+      <Footer />
     </div>
-    <Contact/>
-    <Footer/>
-    </div>
-
   );
 }
 
